83db5642b18a5a7839ae48979fee2e3627b6e380
Weinihow <eddy.yc.wang@gmail.com>
2025-10-03 15:12:01 +0800

    raw dataset

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Training Code in Colab: https://colab.research.google.com/drive/1MjuTPMAj-jrbAFhq2DlyUGF0m0BiuXiT?usp=sharing
+
+Below is how I devided the dataset
+CHB01:
+Train: 01~16
+Val: 17~21
+Test: 22~26
--- main/dataset.py
+++ main/dataset.py
@@ -7,7 +7,7 @@ import librosa
 import numpy as np
 from torch.utils.data import Dataset
 
-def eeg_to_mfcc(eeg_segment, sr=256, n_mfcc=13, n_fft=256, hop_length=13):
+def eeg_to_mfcc(eeg_segment, fs=256, n_mfcc=13, n_fft=256, hop_length=13):
     '''
     This function transform EEG segmants to MFCC
     input : eeg_segment: shape (n_channels, signal_length)
@@ -18,7 +18,7 @@ def eeg_to_mfcc(eeg_segment, sr=256, n_mfcc=13, n_fft=256, hop_length=13):
         channel_data = np.asarray(channel_data, dtype=np.float32)
         mfcc = librosa.feature.mfcc(
             y=np.asarray(channel_data, dtype=np.float32),
-            sr=sr,
+            fs=fs,
             n_mfcc=n_mfcc,
             n_fft=n_fft,
             hop_length=hop_length
@@ -26,7 +26,7 @@ def eeg_to_mfcc(eeg_segment, sr=256, n_mfcc=13, n_fft=256, hop_length=13):
         mfccs.append(mfcc)
     return np.stack(mfccs)
 
-def sliding_window(eeg_segment, seizure_ontime, duration=10, overlap=2, pre_ict=6, sr=256):
+def sliding_window(eeg_segment, seizure_ontime, duration=10, overlap=2, pre_ict=6, fs=256):
     '''
     This function segment the EEG signals and transform into MFCC
     input : eeg_segment      : shape (n_channels, signal_length) -------- original signal
@@ -34,7 +34,7 @@ def sliding_window(eeg_segment, seizure_ontime, duration=10, overlap=2, pre_ict=
             duration         : window duration (sec)
             overlap          : window overlap (sec)
             pre_ict          : define pre-ictal range (min)
-            sr               : sampling rate
+            fs               : sampling rate
     output: features         : shape (data#, n_channels, n_mfcc, time) -- stack of MFCC data
             labels           : shape (data#, )
     '''
@@ -42,8 +42,8 @@ def sliding_window(eeg_segment, seizure_ontime, duration=10, overlap=2, pre_ict=
     labels = []
     pre_ictal_time = max(0, seizure_ontime - pre_ict*60)
 
-    window_size = duration * sr
-    overlap_size = overlap * sr
+    window_size = duration * fs
+    overlap_size = overlap * fs
     stride = window_size - overlap_size
     total_time = eeg_segment.shape[1]
 
@@ -58,7 +58,7 @@ def sliding_window(eeg_segment, seizure_ontime, duration=10, overlap=2, pre_ict=
         if segment.shape[1] != window_size:
           continue
         mfcc = eeg_to_mfcc(segment)
-        label = 1 if start >= pre_ictal_time * sr and start <= seizure_ontime * sr else 0
+        label = 1 if start >= pre_ictal_time * fs and start <= seizure_ontime * fs else 0
         features.append(mfcc)
         labels.append(label)
 
@@ -93,10 +93,72 @@ class MFCCDataset(Dataset):
     def __getitem__(self, idx):
         return self.data[idx], self.label[idx]
     
+def raw_window(segment, info, duration=5, fs=256):
+    '''
+    segment: raw eeg segments (shape: (n_channel, signal_length))
+    info: dict with "interictal_start / end time" / "preictal_start / end time"
+    '''
+    data = []
+    label = []
+    samples_per_segment = fs * duration
+
+    if "interictal_start_time" in info:
+        start = info['interictal_start_time']*fs
+        end = info['interictal_end_time']*fs
+        trimmed_signal = segment[start:end]
+
+        n_segments = len(trimmed_signal) // samples_per_segment
+
+        n_data = []
+        n_label = []
+        for i in range(n_segments):
+            start = i * samples_per_segment
+            end = start + samples_per_segment
+            n_data.append(trimmed_signal[start:end])
+            n_label.append(0)
+        
+        data.extend(n_data)
+        label.extend(n_label)
+
+    if "preictal_start_time" in info:
+        start = info['preictal_start_time']*fs
+        end = info['seizure_start_time']*fs
+        trimmed_signal = segment[start:end]
+
+        n_segments = len(trimmed_signal) // samples_per_segment
+
+        n_data = []
+        n_label = []
+        for i in range(n_segments):
+            start = i * samples_per_segment
+            end = start + samples_per_segment
+            n_data.append(trimmed_signal[start:end])
+            n_label.append(1)
+        
+        data.extend(n_data)
+        label.extend(n_label)
+
+    return data, label
+    
 class RawDataset(Dataset):
     def __init__(self, eeg_signal, timepoints):
         try:
-            
+            self.data = []
+            self.label = []
+            for i, segment in enumerate(eeg_signal):
+                _, info = timepoints[i]
+                data, label = raw_window(segment=segment, info=info, duration=5, fs=256)
+                self.data.extend(data)
+                self.label.extend(label)
+            if len(self.data) != len(self.label):
+                print('data number and label number mismatch')
+                raise 
         except:
             print('dataset loading error')
             raise
+    
+    def __len__(self):
+        return len(self.label)
+    
+    def __getitem__(self, idx):
+        return self.data[idx], self.label[idx]
